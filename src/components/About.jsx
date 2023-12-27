fb7837ee4a6130e694451b3229cfebf86f280b09
Shubhanshu Saxena <84221299+shubhu2002@users.noreply.github.com>
2023-12-27 15:59:17 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import resume from "../assets/resume.pdf"
 import profile from "../assets/profile.jpg";
 
 const About = () => {
